e172fd3c7c5c7412c0ef97c92cf0c7e67ce4fe9b
bgorkem <bgorkem@hotmail.com>
2021-07-26 16:33:58 +0100

    Fix the declaration merging

--- src/client/typings/fin.d.ts
+++ src/client/typings/fin.d.ts
@@ -1,27 +1,34 @@
 
+/** Pull the original fin.me type */
+type Fin =import('openfin/_v2/api/fin').default;
+type FinMe=Fin['me']
 declare namespace fin {
-
-
+  
+    /**Context with id, type and name */
     export type Context = {
         id: Record<string, any>,
         type: string,
         name?: string
     }
 
+    /** subscription returns unsubscribe method*/
     export type Subscription = {
         unsubscribe: ()=>void
     }
     
     type ContextHandler = (context: Context) => void;
 
-    /** Augmented, overridden API for interop  */
-    var me: {
-        interop: {
+    /** Augmented fin.me type with interop*/
+    var me: FinMe  & {
+        interop: {            
             /** Add context handler experimental */
             addContextHandler: (onContext: ContextHandler, type: string) => Promise<Subscription>,
+            
+            /** Set context, experimental */
             setContext: (context: Context) => void;
         }
     }
+
 
 
 
